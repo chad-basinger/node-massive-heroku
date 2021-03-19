138375865d6bca01babf01540ac89558987e15a1
Chad Basinger <chad.basingeris@gmail.com>
2021-03-19 15:04:49 -0600

    fixed and working

--- server/controller.js
+++ server/controller.js
@@ -1,20 +1,23 @@
 module.exports = {
     getItems: (req, res) => {
-        //http://localhost:3040/api/items
-        const db = req.app.get('db')
+        const db = req.app.get('db');
 
         db.get_all_items()
-        .then(dbres => {
-            res.status(200).send(dbres)
-        })
-        .catch(err => console.log(err))
+            .then(dbres => {
+                res.status(200).send(dbres)
+            })
+            .catch(err => console.log(err))
     },
     addItem: (req, res) => {
-            const db = req.app.get('db')
-            const {itemName, itemSize} = req.body;
+        const db = req.app.get('db');
+        const { itemName, itemSize } = req.body;
+        //test this change
+        //test
 
-            db.add_item(itemName, itemSize)
-            .then()
-            .catch()
+        db.add_item(itemName, itemSize)
+            .then(dbRes => {
+                res.status(200).send(dbRes)
+            })
+            .catch(err => console.log(err))
     }
 }
--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
-const express = require('express')
-const app = express()
-const massive = require('massive')
-const ctrl = require('./controller')
+const express = require('express');
+const app = express();
+const massive = require('massive');
+const ctrl = require('./controller');
 
-const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
-app.use(express.json())
+app.use(express.json());
 
 app.get('/api/items', ctrl.getItems);
 app.post('/api/items', ctrl.addItem);
@@ -15,12 +15,14 @@ massive({
     connectionString: CONNECTION_STRING,
     ssl: {
         rejectUnauthorized: false
-    }
-}).then(dbInstance => {
+    } 
+})
+.then(dbInstance => {
     app.set('db', dbInstance)
 
-    app.listen(SERVER_PORT, () => console.log(`Server is humming at ${SERVER_PORT}`))
-}).catch(err => console.log(err))
+    app.listen(SERVER_PORT, () => console.log(`DB connected & Server rockin out on ${SERVER_PORT}fm`));
+})
+.catch(err => console.log(err));
 
 
 // const SERVER_PORT = 5050;
